481b96fd4fd2a2da839535cdbb3b987597d6802f
AmirAliabadi <amiraliabadi@gmail.com>
2015-08-23 21:02:20 -0700

    updated to prototpye using 3 pots for PID tuning

--- ESC_Arming_with_POT/ESC_Arming_with_POT.ino
+++ ESC_Arming_with_POT/ESC_Arming_with_POT.ino
@@ -10,6 +10,9 @@ Servo esc4;
 #define MOTOR_PIN1 9
 
 #define VELOCITY_INPUT_PIN 0
+#define P_PIN 0
+#define I_PIN 1
+#define D_PID 2
 
 /*
 1024 seemed to be the min value for motor to start
@@ -24,11 +27,18 @@ void setup()
 
 void loop()
 {
-  float pot = map(analogRead(VELOCITY_INPUT_PIN),0,668,0,500.0);
+
+  float p = map(analogRead(P_PIN),0,644,0,10000.0)/1000.0;
+  float i = map(analogRead(I_PIN),0,644,0,10000.0)/1000.0;
+  float d = map(analogRead(D_PID),0,644,0,10000.0)/1000.0;
+
+  Serial.print((int)(p*10.0+.5)/10.0);Serial.print("\t");
+  Serial.print((int)(i*10.0+.5)/10.0);Serial.print("\t");
+  Serial.print((int)(d*10.0+.5)/10.0);Serial.println("\t");
  
-  Serial.println(MIN_SIGNAL+pot,4);
+  //Serial.println(MIN_SIGNAL+pot,4);
   
-  esc1.writeMicroseconds(MIN_SIGNAL+pot);
+  //esc1.writeMicroseconds(MIN_SIGNAL+pot);
   
   delay(100);
     
