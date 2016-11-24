f7eabed0c2e8466e2956dd106c5afba40e69781e
AmirAliabadi <amiraliabadi@gmail.com>
2016-11-24 11:45:15 -0800

    Now that I have a decent OScope I can see what is going on with the PMW signals.  Working on the best way to do PWM.

--- PWM_Test/pwm_test/pwm_test.ino
+++ PWM_Test/pwm_test/pwm_test.ino
@@ -2,16 +2,16 @@
 
 //#include <Servo.h>
 //#define _ESC_ Servo
+//#define IS_SERVO
 //
 #include "ESC.h"
 #define _ESC_ ESC
-
+#define IS_ESC
 
 int i = 0;
-int foo = + 10;
 int val = 0;
-int last_start_tick = 0;
-int duty_cycle_tick = 0;
+long last_start_tick = 0;
+long duty_cycle_tick = 0;
 
 
 #define MOTOR_PIN_A       3       // setup with 500hz PWM pins
@@ -19,43 +19,77 @@ int duty_cycle_tick = 0;
 #define MOTOR_PIN_C       11       
 #define MOTOR_PIN_D       10 
 
+#define USE_SERVO
+//#define USE_ANALOGWRITE
+
+#ifdef USE_SERVO
 _ESC_ esc_a;
-//_ESC_ esc_b;
-//_ESC_ esc_c;
-//_ESC_ esc_d;
+_ESC_ esc_b;
+_ESC_ esc_c;
+_ESC_ esc_d;
+#endif
 
 void setup() {
   Serial.begin(115200); 
   while (!Serial);
-//  
-//  pinMode(5, OUTPUT);  
 
+#ifdef USE_SERVO
   esc_a.attach(3);
-//  esc_b.attach(9);
-//  esc_c.attach(11);
-//  esc_d.attach(10);  
+  esc_b.attach(9);
+  esc_c.attach(11);
+  esc_d.attach(10);  
+#endif
 
-//  esc_a.writeMicroseconds(1000);
-//  esc_b.writeMicroseconds(1000);
-//  esc_c.writeMicroseconds(1000);
-//  esc_d.writeMicroseconds(1000);      
+#ifdef USE_ANALOGWRITE
+  analogWrite(MOTOR_PIN_A, 1);
+  analogWrite(MOTOR_PIN_B, 1);
+  analogWrite(MOTOR_PIN_C, 1);
+  analogWrite(MOTOR_PIN_D, 1);
+#endif
+
 }
 
+int next = 1;
+int foo = 1;
+long m;
+
 void loop() {
-//  digitalWrite(5, HIGH);
-//  delayMicroseconds(100); // Approximately 10% duty cycle @ 1KHz
-//  digitalWrite(5, LOW);
-//  delayMicroseconds(900);
-//
+  m = millis();
 
-    val = analogRead(3);    // read the input pin
+  if( (m - duty_cycle_tick) > 1 )
+  {
+    duty_cycle_tick = m;
+    next = next + foo;
 
-    Serial.println(val);
+    do_it();
+    
+    if( next == 255 ) foo = foo *  -1;
+    if( next == 1 ) foo = foo * -1;
+  }
+}
 
-    esc_a.writeMicroseconds(1000 + val * 1.5);
-////    esc_b.writeMicroseconds(i);
-////    esc_c.writeMicroseconds(i);
-////    esc_d.writeMicroseconds(i); 
-//  }
+void do_it()
+{
+#ifdef USE_SERVO
+#ifdef IS_SERVO
+  esc_a.writeMicroseconds( next * 10);
+  esc_b.writeMicroseconds( next * 10);
+  esc_c.writeMicroseconds( next * 10);
+  esc_d.writeMicroseconds( next * 10);
+#endif
+#ifdef IS_ESC
+  esc_a.writeMicroseconds( next );
+  esc_b.writeMicroseconds( next );
+  esc_c.writeMicroseconds( next );
+  esc_d.writeMicroseconds( next );
+#endif  
+#endif
+
+#ifdef USE_ANALOGWRITE
+    analogWrite(MOTOR_PIN_A, next);
+    analogWrite(MOTOR_PIN_B, next);
+    analogWrite(MOTOR_PIN_C, next);
+    analogWrite(MOTOR_PIN_D, next); 
+#endif  
 }
 
