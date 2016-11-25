eb03d486bb3d6ebe46b6aeaa248f9766f724e57a
AmirAliabadi <amiraliabadi@gmail.com>
2016-11-24 19:55:01 -0800

    tested out 3 different pwm methods, analogWrite, Servo.h and ESC.h. Going to try analogWrite in the quadcopter code bases.  it seems the fastest but only has 8bit resolution so may not work.

--- PWM_Test/pwm_test/pwm_test.ino
+++ PWM_Test/pwm_test/pwm_test.ino
@@ -1,28 +1,38 @@
-//#include <SpacebrewYun.h>
 
-//#include <Servo.h>
-//#define _ESC_ Servo
-//#define IS_SERVO
-//
-#include "ESC.h"
-#define _ESC_ ESC
-#define IS_ESC
-
-int i = 0;
-int val = 0;
-long last_start_tick = 0;
-long duty_cycle_tick = 0;
-
-
-#define MOTOR_PIN_A       3       // setup with 500hz PWM pins
+#define MOTOR_PIN_A       3       
 #define MOTOR_PIN_B       9       
 #define MOTOR_PIN_C       11       
 #define MOTOR_PIN_D       10 
 
-#define USE_SERVO
+//#define USE_SERVO
+#define USE_ESC
 //#define USE_ANALOGWRITE
 
+#ifdef USE_ANALOGWRITE
+#define PWM_INCREMENT 1
+#define pwm_20_percent 200
+#define pwm_10_percent 1
+#endif
+
 #ifdef USE_SERVO
+//#include <Servo.h>
+//#define _ESC_ Servo
+#define SERVO_MODE "Servo "
+#define PWM_INCREMENT 10
+#define pwm_20_percent 2000
+#define pwm_10_percent 1000
+#endif
+
+#ifdef USE_ESC 
+#include "ESC.h"
+#define _ESC_ ESC
+#define SERVO_MODE "ESC "
+#define PWM_INCREMENT 10
+#define pwm_20_percent 2000
+#define pwm_10_percent 1000
+#endif
+
+#ifdef SERVO_MODE
 _ESC_ esc_a;
 _ESC_ esc_b;
 _ESC_ esc_c;
@@ -32,15 +42,24 @@ _ESC_ esc_d;
 void setup() {
   Serial.begin(115200); 
   while (!Serial);
+  
+#ifdef SERVO_MODE
+Serial.println(SERVO_MODE);
 
-#ifdef USE_SERVO
-  esc_a.attach(3);
-  esc_b.attach(9);
-  esc_c.attach(11);
-  esc_d.attach(10);  
+esc_a.attach(MOTOR_PIN_A);
+esc_b.attach(MOTOR_PIN_B);
+esc_c.attach(MOTOR_PIN_C);
+esc_d.attach(MOTOR_PIN_D);  
+
+esc_a.writeMicroseconds( pwm_10_percent );
+esc_b.writeMicroseconds( pwm_10_percent );
+esc_c.writeMicroseconds( pwm_10_percent );
+esc_d.writeMicroseconds( pwm_10_percent );
+
 #endif
 
 #ifdef USE_ANALOGWRITE
+  Serial.println("analogWrite Mode");
   analogWrite(MOTOR_PIN_A, 1);
   analogWrite(MOTOR_PIN_B, 1);
   analogWrite(MOTOR_PIN_C, 1);
@@ -49,47 +68,49 @@ void setup() {
 
 }
 
-int next = 1;
-int foo = 1;
-long m;
+unsigned long tick;
+unsigned long last_tick = 0;
 
+int pwm_output = pwm_10_percent;
+int pwm_increment = PWM_INCREMENT;
+int top_hold = 100;
 void loop() {
-  m = millis();
+  tick = millis();
 
-  if( (m - duty_cycle_tick) > 1 )
+  if( (tick - last_tick) > 10 )
   {
-    duty_cycle_tick = m;
-    next = next + foo;
+    last_tick = tick;
+    
+    pwm_output = pwm_output + pwm_increment;
 
     do_it();
     
-    if( next == 255 ) foo = foo *  -1;
-    if( next == 1 ) foo = foo * -1;
+    if( pwm_output >= pwm_20_percent ) { pwm_increment = pwm_increment * -1; delay(2500); }
+    if( pwm_output <= pwm_10_percent ) { pwm_increment = pwm_increment * -1; delay(2500); }
   }
 }
 
 void do_it()
 {
-#ifdef USE_SERVO
-#ifdef IS_SERVO
-  esc_a.writeMicroseconds( next * 10);
-  esc_b.writeMicroseconds( next * 10);
-  esc_c.writeMicroseconds( next * 10);
-  esc_d.writeMicroseconds( next * 10);
-#endif
-#ifdef IS_ESC
-  esc_a.writeMicroseconds( next );
-  esc_b.writeMicroseconds( next );
-  esc_c.writeMicroseconds( next );
-  esc_d.writeMicroseconds( next );
-#endif  
+  
+#ifdef SERVO_MODE
+  Serial.print(SERVO_MODE);
+  Serial.println( pwm_output );
+
+  esc_a.writeMicroseconds( pwm_output );
+  esc_b.writeMicroseconds( pwm_output );
+  esc_c.writeMicroseconds( pwm_output );
+  esc_d.writeMicroseconds( pwm_output );
 #endif
 
 #ifdef USE_ANALOGWRITE
-    analogWrite(MOTOR_PIN_A, next);
-    analogWrite(MOTOR_PIN_B, next);
-    analogWrite(MOTOR_PIN_C, next);
-    analogWrite(MOTOR_PIN_D, next); 
-#endif  
+  Serial.print("analogWrite: ");
+  Serial.println( pwm_output );
+  analogWrite(MOTOR_PIN_A, pwm_output);
+  analogWrite(MOTOR_PIN_B, pwm_output);
+  analogWrite(MOTOR_PIN_C, pwm_output);
+  analogWrite(MOTOR_PIN_D, pwm_output);   
+#endif
+  
 }
 
