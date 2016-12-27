cf8ba4be8e4604c6178c445490760701a0f8a1bb
AmirAliabadi <amiraliabadi@gmail.com>
2016-12-26 21:03:49 -0800

    I have a working model of PPM using interrup pin 3.  I added cli()/sei() and copied the volatile variable into a local temp variable.  This seems to be working correctly and very reliably.  I'm still supprised the PPM decoder is as simple as I have it.  There is a real chance you could loose the sync frame and read the wrong channels.

--- PPM_Test/PPM_Test.ino
+++ PPM_Test/PPM_Test.ino
@@ -14,16 +14,16 @@ void setup() {
   
 }
 
-unsigned long last_ppm_clock = 99999;
-unsigned long current_ppm_clock = 0;
-unsigned long ppm_dt = 0;
+volatile unsigned long last_ppm_clock = 99999;
+volatile unsigned long current_ppm_clock = 0;
+volatile unsigned long ppm_dt = 0;
 volatile boolean ppm_read = true;
-volatile unsigned int ppm_channel = 0;
-volatile unsigned int ppm_channels[6];
+volatile unsigned short ppm_channel = 0;
+volatile unsigned long ppm_channels[6] = {1500,1500,1500,1500,1500,1500};
 
 void ppmRising() {
-  noInterrupts();
-    ppm_read = false;
+//  noInterrupts();
+  ppm_read = false;
     {
       current_ppm_clock = micros();
       ppm_dt = current_ppm_clock - last_ppm_clock;
@@ -31,36 +31,43 @@ void ppmRising() {
         ppm_channel = 0;
       }
       else {
-        ppm_channels[ppm_channel++] = ppm_dt ;
+        ppm_channels[ppm_channel]=ppm_dt; 
+        ppm_channel++;
+        if( ppm_channel > 6 ) ppm_channel = 0;
       }
       last_ppm_clock = current_ppm_clock;   
     }
-    ppm_read = true;
-  interrupts();    
+  ppm_read = true;
+//  interrupts();    
 }
 
 void loop() 
 {        
-  if( millis() - last_log  > 500 ) 
+  if( millis() - last_log  > 3 ) 
   {
-    read_throttle();
-    Serial.println( throttle );  
+    read_throttle(); 
+    Serial.println( throttle );     
     last_log = millis();
   }
 }
 
 void read_throttle() {
-  if( ppm_read ) {}
-  if( ppm_channels[2] == 123 ) {}
-  return ;
-  
   if( ppm_read ) {
+    cli();
+    unsigned long foo = ppm_channels[2] ;    
+    sei();
     
-    if( ppm_channels[2] < 1490 && throttle > 0 ) {
-     throttle -= 1;
+    if( foo < 1490 && throttle > 0 ) {
+      if( throttle > 1 ) throttle -= 1;
+      else throttle = 0;
     } 
+
+    if( foo < 1200 && throttle > 0 ) {
+      if( throttle > 50 ) throttle -= 50;
+      else throttle = 0;
+    }     
     
-    if ( ppm_channels[2] > 1510 && throttle < 1000 ) {
+    if ( foo > 1510 && throttle < 1000 ) {
      throttle += 1 ;
     }
   }
