3ef90c01b6cda64d472180236b8f44a50db6cf5f
AmirAliabadi <amiraliabadi@gmail.com>
2016-12-27 19:36:04 -0800

    Somehow I have the simplest PPM decoder.  I cannot believe it is as easy as it is.  Must be some catch.  Going to move this to the quadcopter code base and see what happens.

--- PPM_Test/PPM_Test.ino
+++ PPM_Test/PPM_Test.ino
@@ -7,6 +7,7 @@ void setup() {
   while (!Serial);
 
   pinMode(6, OUTPUT);
+  digitalWrite(6, LOW);
 
   throttle = 0;
 
@@ -18,8 +19,9 @@ volatile unsigned long last_ppm_clock = 99999;
 volatile unsigned long current_ppm_clock = 0;
 volatile unsigned long ppm_dt = 0;
 volatile boolean ppm_read = true;
+volatile boolean ppm_sync = false;
 volatile unsigned short ppm_channel = 0;
-volatile unsigned long ppm_channels[6] = {1500,1500,1500,1500,1500,1500};
+volatile unsigned long ppm_channels[7] = {4000,1500,1500,1500,1500,1500,1500};
 
 void ppmRising() {
 //  noInterrupts();
@@ -28,12 +30,16 @@ void ppmRising() {
       current_ppm_clock = micros();
       ppm_dt = current_ppm_clock - last_ppm_clock;
       if( ppm_dt >= 4000 ) {
+        ppm_sync = true;
         ppm_channel = 0;
+        ppm_channels[ppm_channel]=ppm_dt;         
       }
       else {
-        ppm_channels[ppm_channel]=ppm_dt; 
-        ppm_channel++;
-        if( ppm_channel > 6 ) ppm_channel = 0;
+        if( ppm_sync ) {
+          ppm_channel++;
+          if( ppm_channel > 6 ) ppm_sync = false;
+          else ppm_channels[ppm_channel]=ppm_dt; 
+        }
       }
       last_ppm_clock = current_ppm_clock;   
     }
@@ -41,20 +47,27 @@ void ppmRising() {
 //  interrupts();    
 }
 
+unsigned long foo ;
+unsigned long micros_count = 0;
 void loop() 
 {        
-  if( millis() - last_log  > 3 ) 
+  read_throttle();   
+  if( millis() - last_log  > 5 ) 
   {
-    read_throttle(); 
     Serial.println( throttle );     
     last_log = millis();
+  }
+
+  if( micros() - micros_count >= foo ) {
+      digitalWrite( 6, !digitalRead(6) );
+      micros_count = micros();
   }
 }
 
 void read_throttle() {
   if( ppm_read ) {
     cli();
-    unsigned long foo = ppm_channels[2] ;    
+    foo = ppm_channels[3] ;    
     sei();
     
     if( foo < 1490 && throttle > 0 ) {
